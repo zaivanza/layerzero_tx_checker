a79434a8b5e47fb480582bfb645ab621cea86dd7
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2023-04-24 17:29:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -371,7 +371,6 @@ if __name__ == "__main__":
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run())
-    loop.close()
 
     TOTAL = get_data_new()
     results = get_results(TOTAL)
